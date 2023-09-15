71013c229e76746d3cc4d778c788f26ec92756f4
Kushagr Narayan <75263274+Kushagr06@users.noreply.github.com>
2023-09-15 22:54:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+const express=require('express')
+const app=express();
+const http=require('http');
+require('dotenv').config()
+// const {Server}=require('socket.io');
+const cors=require("cors");
+const PORT=process.env.PORT||3001
+const FRONT=process.env.FRONT 
+
+// First the the points connect, the message is sent from front to back and then from back to front(broadcast)
+
+app.use(cors());
+
+const server=http.createServer(app);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: FRONT,
+    // credentials: true,
+  },
+});
+
+
+
+// Listen
+io.on("connection",(socket)=>{
+
+  
+  console.log(`User connected: ${socket.id}`)
+
+  //join a room
+  
+  
+  var room_no;
+  var user_no;
+  var user_name;
+
+  socket.on("join_room",(data,name)=>{
+    user_name=name;
+    if(room_no!=data && room_no!=''){
+      socket.emit("clear_chats");
+      socket.to(room_no).emit("disconnected_user",socket.id,user_name,--user_no);
+    socket.join(data);
+    room_no=data
+    user_no= io.sockets.adapter.rooms.get(data).size;
+    
+    io.to(data).emit("Number",user_no)
+    if( socket.rooms.has(data)){
+    io.timeout(2000).to(data).emit("newUser",socket.id,name)
+    }
+    }
+
+   
+
+  });
+
+ 
+  //broadcast listen message to everyone
+  socket.on("send_m",(data) =>{
+    // socket.broadcast.emit("receive_m",data)=>send everyone
+    // console.log(data)
+    socket.to(data.room).emit("receive_m",data);
+  });
+
+  socket.on("download_chats",(name)=>{
+    console.log(`User ${name} downloaded the chats`)
+    io.timeout(1000).emit("downloading_chats",name)
+  });
+  
+
+  socket.on("disconnecting",()=>{
+    // console.log(room_no)
+    console.log(socket.id+" has Disconnected")
+    socket.to(room_no).emit("disconnected_user",socket.id,user_name,--user_no)
+   
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+  });
+
+});
+
+
+server.listen(PORT,()=>{
+  console.log('Now Active at '+FRONT);
+});
